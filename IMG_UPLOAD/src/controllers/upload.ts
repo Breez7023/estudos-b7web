b9df45916c7eb33a89c0d3e300aa15bbfd395abb
Daniel-Ranghetti <daniel@francoit.com.br>
2026-01-14 19:56:52 -0300

    feat: add image processing and UUID generation for uploads
    - Updated package.json and package-lock.json to include sharp and uuid dependencies.
    - Enhanced upload controller to process uploaded images using sharp and generate unique filenames with UUID.
    - Modified multer configuration to save files directly to a temporary directory instead of disk storage.

--- IMG_UPLOAD/src/controllers/upload.ts
+++ IMG_UPLOAD/src/controllers/upload.ts
@@ -1,7 +1,18 @@
 import { RequestHandler } from "express";
+import sharp from "sharp";
+import { v4 } from "uuid";
+import fs from "node:fs/promises";
 
-export const upload: RequestHandler = (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
+export const upload: RequestHandler = async (req, res) => {
+  if (req.file) {
+    const newName = v4() + ".png";
+    const image = await sharp(req.file.path)
+      .toFormat("png")
+      .toFile("./public/images/" + newName);
+
+    await fs.unlink(req.file.path);
+  } else {
+    console.log("Nenhum arquivo enviado.");
+  }
   res.json({});
 };
--- IMG_UPLOAD/src/libs/multer.ts
+++ IMG_UPLOAD/src/libs/multer.ts
@@ -1,15 +1,19 @@
 import multer from "multer";
 
-const diskStorage = multer.diskStorage({
-  filename: (req, file, cb) => {
-    const prefix = "img-" + Math.floor(Math.random() * 9999);
-    cb(null, prefix + ".jpg");
-  },
-  destination: (req, file, cb) => {
-    cb(null, "./public/uploads/tmp");
-  },
-});
+//SALVAR NA PASTA
+// const diskStorage = multer.diskStorage({
+//   filename: (req, file, cb) => {
+//     const prefix = "img-" + Math.floor(Math.random() * 9999);
+//     cb(null, prefix + ".jpg");
+//   },
+//   destination: (req, file, cb) => {
+//     cb(null, "./public/uploads/tmp");
+//   },
+// });
+
+//SALVAR NA MEMORIA
+// const memStorage = multer.memoryStorage();
 
 export const upload = multer({
-  storage: diskStorage,
+  dest: "./public/tmp",
 });
